230895034b6b5a55f2636632fd929d80d654a812
Masya2606 <mariasyt26@gmail.com>
2024-11-10 20:51:57 +0100

    Add while-loop to calculate cumulative sum

--- src/com/gmail/mariasyt26/JavaBasic/Homework/Homework06/CumulativeTotal.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/Homework06/CumulativeTotal.java
@@ -10,6 +10,14 @@ public class CumulativeTotal {
             System.out.println("Num is " + i + ", sum is " + sum );
         }
 
+        sum = 0; //overloading the variable
+        int i = 1;
+        while (i <= num){
+            sum += i;
+            System.out.println("Num is " + i + ", sum is " + sum);
+            i++;
+        }
+
 
         for (int j = 0; j <= 18; j++){
             System.out.print("-");
