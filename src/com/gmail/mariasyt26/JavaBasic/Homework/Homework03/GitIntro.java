362a2d4cd6db477083fc1f5dd489409571018a85
Masya2606 <mariasyt26@gmail.com>
2024-11-10 18:00:46 +0100

    Initial commit

---
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/Homework03/GitIntro.java
@@ -0,0 +1,7 @@
+package com.gmail.mariasyt26.JavaBasic.Homework.Homework03;
+
+public class GitIntro {
+    public static void main(String[] args) {
+        System.out.println("This is shop online");
+    }
+}
