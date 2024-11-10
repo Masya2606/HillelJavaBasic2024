ed114ccaff0c450aa8b823761bc164031ddf21e0
Masya2606 <mariasyt26@gmail.com>
2024-11-10 20:07:11 +0100

    Take users salary as input and calculate tax amount

--- src/com/gmail/mariasyt26/JavaBasic/Homework/Homework05/Taxation.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/Homework05/Taxation.java
@@ -1,8 +1,25 @@
 package com.gmail.mariasyt26.JavaBasic.Homework.Homework05;
 
 
+import java.util.Scanner;
+
 public class Taxation {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Вкажіть свій дохід для розрахунку суми податків: ");
+        double salary = scanner.nextFloat();
+        double taxAmount;
+
+        if (salary > 0 && salary <= 10_000) {
+            taxAmount = salary * 0.025;
+        } else if (salary > 10_000 && salary <= 25_000) {
+            taxAmount = salary * 0.043;
+        } else {
+            taxAmount = salary * 0.067;
+        }
+
+        System.out.println("Сума податку складає: " + taxAmount);
 
     }
 }
