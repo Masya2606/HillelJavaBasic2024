b7eadb2841a6a9a6c849d01a1733416341ee4e5d
Masya2606 <mariasyt26@gmail.com>
2024-11-10 20:41:41 +0100

    Add for-loop to calculate cumulative sum

--- src/com/gmail/mariasyt26/JavaBasic/Homework/Homework06/CumulativeTotal.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/Homework06/CumulativeTotal.java
@@ -1,4 +1,13 @@
 package com.gmail.mariasyt26.JavaBasic.Homework.Homework06;
 
 public class CumulativeTotal {
+    public static void main(String[] args) {
+        int num = 6;
+        int sum = 0;
+
+        for (int i = 1; i <= num; i++){
+            sum += i;
+            System.out.println("Num is " + i + ", sum is " + sum );
+        }
+    }
 }
