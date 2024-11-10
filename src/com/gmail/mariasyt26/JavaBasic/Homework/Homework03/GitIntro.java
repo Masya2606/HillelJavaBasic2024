af1f5924eb3587546da51d30cc7263a25c1840bb
Masya2606 <mariasyt26@gmail.com>
2024-11-10 18:03:25 +0100

    Change output line to "Shop-online for customers"

--- src/com/gmail/mariasyt26/JavaBasic/Homework/Homework03/GitIntro.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/Homework03/GitIntro.java
@@ -2,6 +2,6 @@ package com.gmail.mariasyt26.JavaBasic.Homework.Homework03;
 
 public class GitIntro {
     public static void main(String[] args) {
-        System.out.println("This is shop online");
+        System.out.println("Shop online for customers");
     }
 }
