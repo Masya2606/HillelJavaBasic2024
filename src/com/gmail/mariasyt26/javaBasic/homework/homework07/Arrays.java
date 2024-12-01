43846d7004883c0aa17ef9acf55ac3724895cfd8
Masya2606 <mariasyt26@gmail.com>
2024-12-01 19:11:40 +0100

    Homework-07 - Create array and fill with random integers

--- src/com/gmail/mariasyt26/javaBasic/homework/homework07/Arrays.java
+++ src/com/gmail/mariasyt26/javaBasic/homework/homework07/Arrays.java
@@ -1,4 +1,14 @@
 package com.gmail.mariasyt26.javaBasic.homework.homework07;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Arrays {
+    public static void main(String[] args) {
+        int [] array = new int [10];
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = ThreadLocalRandom.current().nextInt(-100, 100);
+        }
+    }
 }
+
