5586340d54e30d2ff302f1abe1558a42351b7725
Masya2606 <mariasyt26@gmail.com>
2024-12-01 20:33:49 +0100

    Homework-10 - Add method to square entered number

--- src/com/gmail/mariasyt26/JavaBasic/Homework/homework10/MethodsHomework.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/homework10/MethodsHomework.java
@@ -1,7 +1,21 @@
 package com.gmail.mariasyt26.javaBasic.homework.homework10;
 
+import java.util.Scanner;
+
 public class MethodsHomework {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        //Завдання 1
+
+        System.out.println("1. Введіть ціле число: ");
+        int number = scanner.nextInt();
+        System.out.println( "Квадрат числа " +  number + " дорівнює " + square(number));
+
+    }
+
+    public static int square ( int a ){
+        return a * a;
     }
 }
