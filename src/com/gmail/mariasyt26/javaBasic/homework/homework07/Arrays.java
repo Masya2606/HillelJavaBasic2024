dbce19c4ee636073eb8243624879e802a0ac077c
Masya2606 <mariasyt26@gmail.com>
2024-12-01 19:14:19 +0100

    Homework-07 - Add print method for arrays

--- src/com/gmail/mariasyt26/javaBasic/homework/homework07/Arrays.java
+++ src/com/gmail/mariasyt26/javaBasic/homework/homework07/Arrays.java
@@ -10,5 +10,10 @@ public class Arrays {
             array[i] = ThreadLocalRandom.current().nextInt(-100, 100);
         }
     }
+    public static void print(int[] someArray){
+        for(int i = 0; i < someArray.length; i++){ //індексація починається з нуля, отже пятий елемент масива має індекс чотири, тому строга нерівність
+            System.out.print(someArray[i] + "\t");
+        }
+    }
 }
 
