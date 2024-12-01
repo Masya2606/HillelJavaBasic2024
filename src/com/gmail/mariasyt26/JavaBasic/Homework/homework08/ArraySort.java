c82f31340ddcd425cbbc9bc2fa4e14523bdb584e
Masya2606 <mariasyt26@gmail.com>
2024-12-01 20:24:56 +0100

    Homework-08 - add insertion sort method

--- src/com/gmail/mariasyt26/JavaBasic/Homework/homework08/ArraySort.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/homework08/ArraySort.java
@@ -13,10 +13,30 @@ public class ArraySort {
         System.out.print("Початковий вигляд масиву: ");
         print(array);
 
+        insertionSort(array);
+        System.out.print("Bідсортований масив: ");
+        print(array);
+
+
     }
     public static void print(int[] someArray) {
         for (int i = 0; i < someArray.length; i++) { //індексація починається з нуля, отже пятий елемент масива має індекс чотири, тому строга нерівність
             System.out.print(someArray[i] + "\t");
+        }
+    }
+
+
+    public static void insertionSort(int[] someArray) {
+        for (int i = 1; i < someArray.length; i++) {
+            int currentValue = someArray[i];
+            int j = i - 1;
+            while (j >= 0 && someArray[j] > currentValue) {
+                // Переміщення елементів, які більше за currentValue, на одну позицію вперед
+                someArray[j + 1] = someArray[j];
+                j--;
+            }
+            // Вставлення currentValue на правильну позицію у відсортованій частині масиву
+            someArray[j + 1] = currentValue;
         }
     }
 }
