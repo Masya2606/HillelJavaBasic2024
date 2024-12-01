82a6641827b94163cb6c91698e2c8e7a442392a7
Masya2606 <mariasyt26@gmail.com>
2024-12-01 20:39:29 +0100

    Homework-10 - Overload square-method + add method to calculate cylinder volume

--- src/com/gmail/mariasyt26/JavaBasic/Homework/homework10/MethodsHomework.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/homework10/MethodsHomework.java
@@ -13,9 +13,26 @@ public class MethodsHomework {
         int number = scanner.nextInt();
         System.out.println( "Квадрат числа " +  number + " дорівнює " + square(number));
 
+
+        //Завдання 2
+
+        System.out.println("2. Введіть радіус циліндра: ");
+        double radius = scanner.nextDouble();
+        System.out.println("Введіть висоту циліндра: ");
+        double height = scanner.nextDouble();
+        System.out.println( "Об'єм циліндра радіусом " +  radius + " та висотою " + height + " дорівнює " + cylinderVolume(radius, height));
+
     }
 
     public static int square ( int a ){
         return a * a;
+    }
+
+    public static double square ( double a ){
+        return a * a;
+    }
+
+    public static double cylinderVolume (double radius, double height){
+        return Math.PI * square(radius) * height;
     }
 }
