6cfc486c3dacdccd5dd1ec6102ad8d3929fe425f
Masya2606 <mariasyt26@gmail.com>
2024-12-16 22:46:08 +0100

    Homework-11 - main method to create person in PersonRunner

--- src/com/gmail/mariasyt26/JavaBasic/Homework/homework11/PersonRunner.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/homework11/PersonRunner.java
@@ -1,4 +1,9 @@
 package com.gmail.mariasyt26.javaBasic.homework.homework11;
 
 public class PersonRunner {
+    public static void main(String[] args) {
+        Person john = new Person("John", 30, "інженер");
+        Person mary = new Person("Mary", 25, "вчитель");
+        Person bob = new Person("Bob", 35, "лікар");
+    }
 }
