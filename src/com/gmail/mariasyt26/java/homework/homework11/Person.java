5d05c30f6191997668264144697425617b0a5111
Masya2606 <mariasyt26@gmail.com>
2024-12-16 23:12:54 +0100

    Homework-11 - eliminate all capital letters in packages' names

---
+++ src/com/gmail/mariasyt26/java/homework/homework11/Person.java
@@ -0,0 +1,24 @@
+package com.gmail.mariasyt26.java.homework.homework11;
+
+public class Person {
+    String name;
+    int age;
+    String profession;
+
+    //конструктор
+    public Person ( String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+    public void changeProfession(String newProfession){//можно ли передавать конкретные поля класса как параметры? нужно ли?
+        this.profession = newProfession;
+        System.out.println("\nПісля оновлення професії");
+        describeYourself();
+    }
+
+    public void describeYourself (){
+        System.out.println("Iм'я: " + name + ", Вік: " + age + " Професія: " + profession );
+    }
+}
---
+++ src/com/gmail/mariasyt26/java/homework/homework11/PersonRunner.java
@@ -0,0 +1,15 @@
+package com.gmail.mariasyt26.java.homework.homework11;
+
+public class PersonRunner {
+    public static void main(String[] args) {
+        Person john = new Person("John", 30, "інженер");
+        Person mary = new Person("Mary", 25, "вчитель");
+        Person bob = new Person("Bob", 35, "лікар");
+
+        john.describeYourself();
+        mary.describeYourself();
+        bob.describeYourself();
+
+        mary.changeProfession("архітетор");
+    }
+}
