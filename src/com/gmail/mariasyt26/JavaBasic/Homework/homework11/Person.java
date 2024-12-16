89153026738565e8c084beb2a7f33528b6e74584
Masya2606 <mariasyt26@gmail.com>
2024-12-16 22:41:39 +0100

    Homework-11 - set class fields and create constructor

--- src/com/gmail/mariasyt26/JavaBasic/Homework/homework11/Person.java
+++ src/com/gmail/mariasyt26/JavaBasic/Homework/homework11/Person.java
@@ -1,4 +1,14 @@
 package com.gmail.mariasyt26.javaBasic.homework.homework11;
 
 public class Person {
+    String name;
+    int age;
+    String profession;
+
+    //конструктор
+    public Person ( String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
 }
