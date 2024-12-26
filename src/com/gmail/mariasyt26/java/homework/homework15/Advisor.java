90fcf7c95c06a33944fa8ce143898faa9a6b4d64
Masya2606 <mariasyt26@gmail.com>
2024-12-26 18:59:03 +0100

    Homework-15 - Create Enum "Day" and Interface "Advisor"

---
+++ src/com/gmail/mariasyt26/java/homework/homework15/Advisor.java
@@ -0,0 +1,5 @@
+package com.gmail.mariasyt26.java.homework.homework15;
+
+public interface Advisor {
+    void advise(Day day);
+}
---
+++ src/com/gmail/mariasyt26/java/homework/homework15/Day.java
@@ -0,0 +1,11 @@
+package com.gmail.mariasyt26.java.homework.homework15;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+}
